ad0d440a969f322145f4944c83354969a7cb7d68
mukadem <mukademb@hotmail.com>
2019-03-15 15:42:41 +0100

    2nd Test lol

---
+++ Exercise4_15.03.19.py
@@ -0,0 +1,12 @@
+# Exercise 4: Read a precipitation file
+
+with open(r"C:\Users\mukad\Documents\UNIVERSITÄT\Master\Geographie\FS2019\Geodatenanalyse\Session 4\precip_data.txt") as f:
+    for num in f:
+        print((num.split()[2]))
+    a = str((num.split()[2]))
+    print
+
+
+
+
+# I am writing this pseudocode to test GitKraken
